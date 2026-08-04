fdbbf9719ab6c601099e6111c1f2ef5d0f2d7ca5
alannavarrodiaz13-web <alannavarrodiaz13@gmail.com>
2026-08-04 13:47:50 +0200

    Add files via upload

---
+++ words.js
@@ -0,0 +1,65 @@
+// ========================================
+// LISTAS DE PALABRAS
+// TR Memòria
+// ========================================
+
+// AGOSTO
+const wordsA = [
+
+"arbre",
+"guitarra",
+"lluna",
+"ampolla",
+"camisa",
+"rellotge",
+"muntanya",
+"llimona",
+"cadira",
+"estrella",
+"riu",
+"sabata",
+"finestra",
+"cavall",
+"cullera"
+
+];
+
+
+
+// NOVIEMBRE
+
+const wordsB = [
+
+"avió",
+"taronja",
+"sofà",
+"biblioteca",
+"flor",
+"bicicleta",
+"núvol",
+"mirall",
+"espelma",
+"platja",
+"taula",
+"paraigua",
+"gat",
+"motxilla",
+"campana"
+
+];
+
+
+
+// ========================================
+// CONFIGURACIÓN
+// ========================================
+
+// Cambia a "A" en agosto y a "B" en noviembre
+
+const currentList = "A";
+
+
+
+// Lista utilizada por el experimento
+
+const words = currentList === "A" ? wordsA : wordsB;
